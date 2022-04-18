3b99eb94936d539235ac007e1f5bf96761aa4917
Nifu Dan <dannifu010706@outlook.com>
2022-04-17 21:36:43 -0400

    a4(4)

--- database.js
+++ database.js
@@ -12,16 +12,12 @@ let row = stmt.get();
 
 if (row === undefined) {
 
-    console.log('Your database appears to be empty. I will initialize it now.');
-
     const logdata = `
         CREATE TABLE logdata ( remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
         
     `;
-
     logdb.exec(logdata);
 
-    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
 } else {
 
     console.log('Database exists.')
