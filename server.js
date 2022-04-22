53f8eca62b5b0aa9bbe5d9def93dba81301057ed
Nifu Dan <dannifu010706@outlook.com>
2022-04-22 02:25:43 -0400

    a4

--- server.js
+++ server.js
@@ -10,20 +10,100 @@ args["log"]=true
 args["help"]
 var port = args.port | 5555
 
+
+app.get('/app/echo/:number',(req,res)=>{
+    res.status(200)/json({'message':req.params.number})
+})
+
+function coinFlip() {
+    return Math.random() > .5 ? ("heads") : ("tails")
+  }
+
+function coinFlips(flips) {
+    var arr = [];
+    for(let i =0 ; i<flips;i++){
+      arr.push(coinFlip());
+    }
+   return arr;
+  }
+
+  function countFlips(array) {
+    let result={};
+    array.forEach((item) => { 
+      if(result[item]){
+        result[item]++;
+      }else{
+        result[item]=1;
+      }
+    });
+    return result;
+   }
+
+   function flipACoin(call) {
+  
+    const results = {call: '', flip: '', result: ''}
+    results.call=call;
+    results.flip=coinFlip();
+    if(results.call==results.flip){
+      results.result="win";
+    }
+    else{
+      results.result="lose";
+    }
+    return results;
+    }
+  
+
+
+app.get('/app',(req,res)=> {
+   res.statusCode=200;
+   res.statusMessage='OK';
+   res.writeHead(res.statusCode,{'Content-Type':'text/plain'});
+   res.end(res.statusCode+' '+res.statusMessage)
+
+})
+app.get('/app/flip',(req,res)=>{
+    var flips = coinFlip()
+    
+    res.status(200).json({ 'flip':flip})
+})
+app.get('/app/flips/:number',(req,res)=>{
+    const flips = (req.params.number)
+    const coins=coinFlips(flips)
+    res.status(200).json({ 'raw':coins,'summary':countFlips(coins)})
+})
+
+app.get('/app/flip/call/:call',(req,res)=>{
+const calling =(req.params.call)
+const coin = coinFlip()
+var result ="win"
+if(coin!=calling){
+    result="lose"
+}
+res.status(200).json({"call":calling,"flip":coin,"result":result})
+
+
+})
+app.use(function(req,res){
+    res.status(404).send("Endpoint does not exist")
+    res.type("text/plain")
+})
+
+
 if (args.help) {
-    console.log("Return this message and exit.")
+    console.log(help)
     process.exit(0)
 }
 
 const server = app.listen(port, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",port))
 });
-app.get("/app/", (req, res, next) => {
+app.get("/app/", (req, res) => {
     res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
 
-app.use("/app/new/log", (req, res, next) => {
+app.use( (req, res, next) => {
     let logdata = {
         remoteaddr: req.ip,
         remoteuser: req.user,
@@ -42,7 +122,7 @@ app.use("/app/new/log", (req, res, next) => {
     next()
 });
 
-if(args.debug==true){
+if(args.debug===true){
     app.get("/app/log/access",(req,res) =>{
      
          const stmt = logdb.prepare('SELECT * FROM accesslog').all()
@@ -52,5 +132,11 @@ if(args.debug==true){
      throw new Error('Error test successful.')
  
     })
+    
+const morgan = require("morgan");
+const fs = require("fs");
+const accessLog = fs.createWriteStream("./data/log/access.log", { flags: "a" });
+app.use(morgan("combined", { stream: accessLog }));
+
  
  }
