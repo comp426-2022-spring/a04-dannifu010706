005da2d709f9f95eceeb90e57f11541b0450f311
Nifu Dan <dannifu010706@outlook.com>
2022-04-25 17:12:04 -0400

    a4

--- database.js
+++ database.js
@@ -1,22 +1,32 @@
 
 "use strict";
-const Database = require('better-sqlite3');
+const database = require('better-sqlite3')
 
+const logdb = new database('log.db')
 
-const logdb = new Database('log.db');
-const stmt = logdb.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
-    );
-
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
-
 if (row === undefined) {
-
-    const log = `
-        CREATE TABLE accesslog ( remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
-        
-    `;
-    logdb.exec(log);
+    console.log('Log database appears to be empty. Creating log database...')
+    const sqlInit = `
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT, 
+            remoteuser TEXT, 
+            date TEXT, 
+            method TEXT, 
+            url TEXT, 
+            protocol TEXT,
+            httpversion NUMERIC, 
+            status INTEGER, 
+            content_length NUMERIC,
+            referrer_url TEXT,
+            user_agent TEXT
+        );
+    `
+    logdb.exec(sqlInit)
+} else {
+    console.log('Log database exists.')
 }
 
 module.exports = logdb
